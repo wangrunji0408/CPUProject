f0b24119e08ac11b9f80102a6435bc9fd8bf66ca
Shols <790479613@qq.com>
2017-10-19 11:49:06 +0800

    alu without debug

--- Project2/alu.vhd
+++ Project2/alu.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use ieee.std_logic_arith.all;
 use work.Base.all;
 
 entity ALU is
@@ -21,6 +22,14 @@ begin
 		case( op ) is
 			when "0000" => s <= a + b;
 			when "0001" => s <= a - b;
+            when "0010" => s <= a and b;
+            when "0011" => s <= a or b;
+            when "0100" => s <= a xor b;
+            when "0101" => s <= not a;
+            when "0110" => s <= to_u16(to_bitvector(conv_std_logic_vector(a ,16)) sll conv_integer(b));
+            when "0111" => s <= to_u16(to_bitvector(conv_std_logic_vector(a ,16)) srl conv_integer(b));
+            when "1000" => s <= to_u16(to_bitvector(conv_std_logic_vector(a ,16)) sra conv_integer(b));
+            when "1001" => s <= to_u16(to_bitvector(conv_std_logic_vector(a ,16)) rol conv_integer(b));
 			when others => s <= to_unsigned(0, 16);
 		end case ;
 	end process ; -- calc
