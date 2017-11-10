70f051bd91e646c36a4681279a381da614261ca2
WangRunji <wangrunji0408@163.com>
2017-11-10 11:03:06 +0800

    Add init files.

---
+++ Project4/top.vhd
@@ -0,0 +1,41 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.Base.all;
+
+entity Top is
+	port (
+		clk, rst: in std_logic;
+		clk11, clk50: in std_logic;
+		switch: in u16;
+		light: out u16;
+		
+		ram1addr, ram2addr: out u18;
+		ram1data, ram2data: inout u16;
+		ram1read, ram1write, ram1enable: out std_logic;
+		ram2read, ram2write, ram2enable: out std_logic;
+
+		uart_data_ready, uart_tbre, uart_tsre: in std_logic;	-- UART flags 
+		uart_read, uart_write: out std_logic;					-- UART lock
+
+		ps2_clk, ps2_data: in std_logic;
+
+		vga_r, vga_g, vga_b: out u3;
+		vga_vs, vga_hs: out std_logic;
+
+		digit0raw, digit1raw: out std_logic_vector(6 downto 0);
+		key: in std_logic_vector(3 downto 0)
+	) ;
+end Top;
+
+architecture arch of Top is	
+	signal digit0, digit1: u4;
+begin
+
+	digit0raw <= DisplayNumber(digit0);
+	digit1raw <= DisplayNumber(digit1);
+
+	vga_r <= "000"; vga_g <= "000"; vga_b <= "000"; vga_vs <= '1'; vga_hs <= '1'; 
+
+	
+end arch ; -- arch
