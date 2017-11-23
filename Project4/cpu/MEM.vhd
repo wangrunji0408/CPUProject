930c4797395a636d0cc7ca1aeee66b54066af873
ommyzhang <tdxdxoz@gmail.com>
2017-11-23 20:49:41 +0800

    seems to finish mem

--- Project4/cpu/MEM.vhd
+++ Project4/cpu/MEM.vhd
@@ -8,16 +8,16 @@ entity MEM is
 	port (		
 		------ 对外接口 ------
 
-		mem_type: out MEMType;
-		mem_addr: out u16;
-		mem_write_data: out u16;
+		mem_type: out MEMType; --
+		mem_addr: out u16;     --
+		mem_write_data: out u16; --
 		mem_read_data: in u16;
 		mem_busy: in std_logic;	-- 串口操作可能很慢，busy=1表示尚未完成
 
 		------ 输出到Ctrl ------
 
 		-- 当读串口时，请求暂停，直到data_ready
-		stallReq: out std_logic;
+		stallReq: out std_logic; --
 
 		------ 从EX读入 ------
 
@@ -39,7 +39,54 @@ begin
 	mem_type <= ReadRam1;
 	mem_addr <= x"0000";
 	mem_write_data <= x"0000";
-	stallReq <= mem_busy;
-	writeRegOut <= writeReg;
+	stallReq <= mem_busy when isLW = '1' or isSW = '1';
+
+	writeRegOut.enable <= writeReg.enable;
+	writeRegOut.addr   <= writeReg.addr;
+
+	mem_type <= ReadUart when isLW = '1' and aluOut = x"bf00" else
+			   WriteUart when isSW = '1' and aluOut = x"bf00" else
+			   ReadRam1 when isLW = '1' and aluOut < x"8000" else
+			   ReadRam2 when isLW = '1' and aluOut < x"b000" else
+			   WriteRam1 when isSW = '1' and aluOut < x"8000" else
+			   WriteRam2 when isSW = '1' and aluOut < x"b000" else
+			   None;
+	mem_addr <= aluOut;
+	writeRegOut.data <= mem_read_data when isLW = '1' else 
+						(0=>mem_busy, 1=>mem_busy, others=>'0') when isLW = '1' and aluOut = x"bf01" else 
+						aluOut;
+	mem_write_data <= writeReg.data when isSW = '1';
+
+
+--	if isLW = '1'
+--	then
+--		if aluOut > x"bf01"
+--		then 
+--			mem_type = None;
+--		elsif aluOut = x"bf01"
+--		then
+--			mem_type = None;
+--			writeRegOut.data <= (0=>mem_busy, others=>'0');
+--		elsif aluOut = x"bf00"
+--		then
+--			mem_type = ReadUart;
+--			writeRegOut.data <= mem_read_data;
+--		else
+--			mem_type = ReadRam1 when aluOut<x"8000" else ReadRam2;
+--			mem_addr <= aluOut;
+--			writeRegOut.data <= mem_read_data;
+--		end if;
+--	elsif isSW = '1'
+--	then
+--		if aluOut > x"bf00"
+--		then
+--			mem_type = None;
+--		elsif aluOut = x"bf00"
+--		then
+--			mem_type = WriteUart;
+--			mem_write_data <= writeReg.data;
+--		end if;
+--	end if;
+--			
 
 end arch ; -- arch
