0864c852de2984cd4bcb2de63d92f5a579ad799d
ommyzhang <tdxdxoz@gmail.com>
2017-11-30 15:16:09 +0800

    add end_addr

--- Project4/device/Boot.vhd
+++ Project4/device/Boot.vhd
@@ -6,7 +6,7 @@ use work.Base.all;
 entity Boot is
 	port(
 		rst, clk: in std_logic; -- rst is not real rst. give me rst=0 when want me work
-		start_addr : in u16;
+		start_addr, end_addr : in u16;
 		-- FLASH --
 		flash_addr: out u16; -- 22 downto 1, first 6 block
 		flash_data: inout u16;
@@ -25,7 +25,6 @@ architecture arch of Boot is
 	signal now_addr:u16;
 	signal status: integer := 0;
 	signal finish: std_logic;
-	constant end_addr:u16 := x"0220";
 
 begin
 	CE0 <= '0';
